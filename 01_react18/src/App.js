61b17d4f5b4727c0ba87a1015aacd8eb7d73165b
noentoro <nicholasoentoro@gmail.com>
2024-01-15 22:33:57 -0800

    fetch wordpress api + render post contents

--- 01_react18/src/App.js
+++ 01_react18/src/App.js
@@ -1,19 +1,28 @@
-function App() {
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch(
+        "https://knightyb.000webhostapp.com/wp-json/wp/v2/posts"
+      )
+
+      const posts = await response.json();
+      setPosts(posts);
+
+    }
+    fetchPosts();
+  },[]);
   return (
-    <div className="App">
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {posts.map((post, index) => (
+        <div key={index}>
+          <p dangerouslySetInnerHTML={{__html: post.title.rendered}}/>
+          <p dangerouslySetInnerHTML={{__html: post.content.rendered}}/>
+        </div>
+        ))}
     </div>
   );
 }
